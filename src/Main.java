8364a5008b7174627838b50f9393fe3ac4130f35
Omar Ahmed El-Gazzar <92094226+OmmrGAZAR315@users.noreply.github.com>
2023-12-22 17:45:55 +0200

    ctrl+al+l

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,28 @@ public class Main {
 
         //Simple Coffee
         Beverage simple_coffee = new Coffee();
-        System.out.println(simple_coffee.getDescription() + " " + simple_coffee.cost()+"EGP ");
+        System.out.println(simple_coffee.getDescription() + " " + simple_coffee.cost() + "EGP ");
 
         System.out.println();
 
         //Simple Tea
         Beverage simple_tea = new Tea();
-        System.out.println(simple_tea.getDescription() + " " + simple_tea.cost()+"EGP ");
+        System.out.println(simple_tea.getDescription() + " " + simple_tea.cost() + "EGP ");
 
         System.out.println();
 
 
         //Tea with milk
         Beverage tea_milk = new MilkDecorator(new Tea());
-        System.out.println(tea_milk.getDescription() + " " + tea_milk.cost()+"EGP ");
+        System.out.println(tea_milk.getDescription() + " " + tea_milk.cost() + "EGP ");
 
         System.out.println();
 
         //Tea with honey and milk
         Beverage Tea_honey_milk = new HoneyDecorator(new MilkDecorator(new Tea()));
-        System.out.println(Tea_honey_milk.getDescription() + " " + Tea_honey_milk.cost()+"EGP ");
+        System.out.println(Tea_honey_milk.getDescription() + " " + Tea_honey_milk.cost() + "EGP ");
 
         System.out.println();
 
     }
-
 }
